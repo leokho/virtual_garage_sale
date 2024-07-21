8ec74cdca3912b48d0bbcc7271c2cb46f29070a4
leokho <leokho@gmail.com>
2024-07-22 01:35:00 +0200

    Fast forward to latest version

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, redirect, url_for, render_template, Response
+from functools import wraps
+from flask import Flask, request, redirect, url_for, render_template, Response, send_from_directory
+from PIL import Image
 import os
 import csv
+import re
+
+# CHANGE ME (or use env. variables)
+USERNAME = os.environ.get('GARAGE_SALE_USERNAME', 'admin')
+PASSWORD = os.environ.get('GARAGE_SALE_PASSWORD', 'password')
+VALID_STATUSES = {'Published', 'Reserved', 'Sold'}
+
+IMAGE_SIZE = (500, 500)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-
-# CHANGE ME (or use env. variables)
-USERNAME = 'admin'
-PASSWORD = 'password'
-
+app.config['IMAGE_FOLDER'] = 'uploads/images'
 
 # Create the upload folder if it doesn't exist
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
@@ -18,8 +24,34 @@ def parse_csv(filepath):
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            items.append(row)
+            if row['Status'] in VALID_STATUSES:
+                items.append(row)
     return items
+
+def save_image(file, internal_id, image_number):
+    img = Image.open(file)
+    img.thumbnail((500, 500))
+    img.save(os.path.join(app.config['IMAGE_FOLDER'], f'{internal_id}_{image_number}.jpg'))
+
+
+def get_item_images():
+    """Assumption: File format is <ID>_<nr>.jpg"""
+    images = {}
+    pattern = r"^(?P<name>.*?)_(?P<nr>\d+)\.jpg$"
+
+    for filename in os.listdir(app.config['IMAGE_FOLDER']):
+        match = re.match(pattern, filename)
+
+        if match:
+            internal_id = match.group("name")
+            image_number = match.group("nr")
+
+            if internal_id not in images:
+                images[internal_id] = []
+            images[internal_id].append(filename)
+
+    return images
+
 
 def check_auth(username, password):
     return username == USERNAME and password == PASSWORD
@@ -31,6 +63,7 @@ def authenticate():
         {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 def requires_auth(f):
+    @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
@@ -39,34 +72,40 @@ def requires_auth(f):
     return decorated
 
 
-@app.route('/')
+
+@app.route('/', endpoint='index')
 def index():
     items = parse_csv(os.path.join(app.config['UPLOAD_FOLDER'], 'items.csv'))
-    return render_template('index.html', items=items)
+    images = get_item_images()
+    return render_template('index.html', items=items, images=images)
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/admin', endpoint='admin')
+@requires_auth
+def admin():
+    success = request.args.get('success', 0)
+    images = sorted(os.listdir(app.config['IMAGE_FOLDER']))
+    return render_template('upload.html', images=images, success=int(success))
+
+
+@app.route('/admin/upload', methods=['POST'], endpoint='upload_file')
 @requires_auth
 def upload_file():
-    if request.method == 'POST':
-        # Check if the post request has the file part
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'items.csv')
-            file.save(filepath)
-            return redirect(url_for('index'))
-    return '''
-    <!doctype html>
-    <title>Upload CSV</title>
-    <h1>Upload new CSV</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    files = request.files.getlist('file')
+    for file in files:
+        if file.filename.endswith('.csv'):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'items.csv'))
+        elif file and '_' in file.filename:
+            parts = file.filename.rsplit('_', 1)
+            internal_id = parts[0]
+            image_number = parts[1].split('.')[0]
+            save_image(file, internal_id, image_number)
+    return redirect(url_for('admin', success=1))
+
+@app.route('/imgs/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['IMAGE_FOLDER'], filename)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, port=8000)
